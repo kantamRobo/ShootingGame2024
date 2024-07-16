050336478c3d022895002e41c7fbdb0ee9953eee
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-07-16 15:30:24 +0900

    カメラの初期化を実装

--- ShootingGame3D/Camera.cpp
+++ ShootingGame3D/Camera.cpp
@@ -1,5 +1,15 @@
 #include "Camera.h"
 
-void Camera::Init()
+void Camera::Init(const VECTOR& playerpos,const VECTOR& enemypos)
+{
+
+	auto target =VGet((playerpos.x + enemypos.x) / 2, position.y, (playerpos.x + enemypos.x) / 2);
+	
+	SetCameraNearFar(100.0f, 15000.0f);
+	SetCameraPositionAndTarget_UpVecY(position, target);
+
+}
+
+void Camera::Update()
 {
 }
--- ShootingGame3D/Camera.h
+++ ShootingGame3D/Camera.h
@@ -2,12 +2,15 @@
 
 class Camera {
 
-	void Init();//基本は固定カメラとして使う。
+public:
+	void Init(const VECTOR& playerpos, const VECTOR& enemypos);//基本は固定カメラとして使う。
 
 	void Update(); //Imguiでの編集に使用。位置調整
 
 	VECTOR position; //Imguiで調整
 
 	VECTOR target;
+
+	
 
 };
