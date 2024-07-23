f0bedf1f5bf6449ac62899cf8206034e2a48da6c
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-07-23 11:10:09 +0900

    微修正

--- ShootingGame3D/Camera.cpp
+++ ShootingGame3D/Camera.cpp
@@ -16,22 +16,23 @@ void Camera::Update(char* input)
 		const float  CAMERA_LOOK_AT_HEIGHT = 400.0f;
 		// カメラのクリッピング距離を設定
 		SetCameraNearFar(100.0f, 10000.0f);
-		VECTOR CameraLookAtPosition;
-		CameraLookAtPosition.y = CAMERA_LOOK_AT_HEIGHT;
-
 		VECTOR TempPosition1;
 
 
 
 
 		// 注視点はキャラクターモデルの座標から CAMERA_LOOK_AT_HEIGHT 分だけ高い位置
-		CameraLookAtPosition = position;
-
+		
 		
 		VECTOR TempPosition2;
 		TempPosition2 = VGet(200, 200, 500);
 		//TempPosiiton2の位置がフレームごとに加算されているので、ここの算出を設計する
 		// 算出した座標に注視点の位置を加算したものがカメラの位置
+		VECTOR CameraLookAtPosition;
+		CameraLookAtPosition.y = CAMERA_LOOK_AT_HEIGHT;
+		CameraLookAtPosition = position;
+
+
 		position = VAdd(TempPosition2, CameraLookAtPosition);
 
 	
