7dabc60e551bb0b320b243a5e6c6125dcc27d3d3
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-07-23 11:39:43 +0900

    微修正

--- ShootingGame3D/Camera.cpp
+++ ShootingGame3D/Camera.cpp
@@ -5,19 +5,53 @@ void Camera::Init(const VECTOR& playerpos,const VECTOR& enemypos)
 	const float  CAMERA_LOOK_AT_HEIGHT = 400.0f;
 	
 	CameraLookAtPosition.y += CAMERA_LOOK_AT_HEIGHT;
-
+	 temppos = VGet(0, 0, 0);
 	position = VGet(0, 0, 0);
+	CameraLookAtPosition = playerpos;
 	//Imguiの実装
 }
 
 void Camera::Update(char* input)
 {
 
+	
+
+
+	if (input[KEY_INPUT_UP] == 1)
+	{
+		
+			temppos.y -= 5;
+			
+		
+	}
+
+	if (input[KEY_INPUT_DOWN] == 1)
+	{
+		
+		temppos.y += 5;
+			//posY += 5;
+		
+	}
+
+	if (input[KEY_INPUT_LEFT] == 1) {
+	
+		temppos.x -= 100;
+		
+	}
+
+	if (input[KEY_INPUT_RIGHT] == 1)
+	{
+		
+		temppos.x += 100;
+
+		
+	}
+	//キーを離すと映らなくなる
 	//TODO: カメラを手動で回転させてオブジェクトのほうを向かせる
 	CameraLookAtPosition = position;
 	{
 	
-		position.x += 20;
+		position = temppos;
 
 	
 		
--- ShootingGame3D/Camera.h
+++ ShootingGame3D/Camera.h
@@ -16,4 +16,6 @@ public:
 
 	VECTOR CameraLookAtPosition;
 
+	VECTOR temppos;
+
 };
