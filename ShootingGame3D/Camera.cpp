f46131262917a18d8f15cb4027a678936a82eb7f
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-07-26 10:32:18 +0900

    微修正

--- ShootingGame3D/Camera.cpp
+++ ShootingGame3D/Camera.cpp
@@ -11,8 +11,8 @@ void Camera::Init(const VECTOR& playerpos,const VECTOR& enemypos)
 
 void Camera::Update(char* input)
 {
-
-	
+	SetBackgroundColor(0, 0, 255); // ê‘0, óŒ0, ê¬255
+	SetUseZBuffer3D(TRUE);
 
 
 	if (input[KEY_INPUT_UP] == 1)
