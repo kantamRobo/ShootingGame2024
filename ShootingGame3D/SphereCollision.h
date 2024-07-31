58c6bebb6de8bc1bf0770429413575c3859004a6
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-07-31 14:23:20 +0900

    微修正

--- ShootingGame3D/SphereCollision.h
+++ ShootingGame3D/SphereCollision.h
@@ -3,11 +3,11 @@ class Sphere
 {
 public:
 	VECTOR position;
-	float radius;
+	float radius=150;
 	static    bool Intersect(const Sphere& r1, const Sphere& r2);
 void DrawDebug(const VECTOR& objectpos)
 {
-    DrawSphere3D(objectpos, 150, 5, GetColor(0, 255, 0), GetColor(0, 255, 0), FALSE);
+    DrawSphere3D(objectpos, radius, 5, GetColor(0, 255, 0), GetColor(0, 255, 0), FALSE);
 }
 };
 
