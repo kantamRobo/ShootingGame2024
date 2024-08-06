9ad720b83fa178f9ef5ca6e0dabb71e881aa9b3c
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-08-06 10:18:49 +0900

    プレイヤーの動きと敵の動きのたたき台ができた。

--- ShootingGame3D/Player3.cpp
+++ ShootingGame3D/Player3.cpp
@@ -21,13 +21,50 @@ void Player3::Update(char* input)
 	Draw3D(position);
 
 	sphere.DrawDebug(position);
-	if (input[KEY_INPUT_Z])
-	{
+	     if (input[KEY_INPUT_UP] == 1)
+		{
 
+			position.y -= 5;
+
+
+		}
+
+		if (input[KEY_INPUT_DOWN] == 1)
+		{
+
+			position.y += 5;
+			//posY += 5;
+
+		}
+
+		if (input[KEY_INPUT_LEFT] == 1) {
+
+			position.x -= 5;
+
+		}
+
+		if (input[KEY_INPUT_RIGHT] == 1)
+		{
+
+			position.x += 5;
+
+
+		}
+
+		if (input[KEY_INPUT_Q] == 1)
+		{
+			position.z += 5;
+		}
+
+		if (input[KEY_INPUT_Z] == 1)
+		{
+			position.z -= 5;
+		}
+		if(input[KEY_INPUT_A] == 1)
 		Attack3D();
 
 
-	}
+	
 	
 }
 
